41b97577fbc7e54f952c1ec02bf3cd6b3d6b5935
Imagine Chiu <chihfan.chiu@igroup.tw>
2019-11-24 03:30:05 +0800

    新增 bootstrap 範例

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ https://nextjs.org/docs
 ## Using IDE Setting (WebStorm)
 
 1. `.next` folder search file index is slow,
-so you can set `.next` folder `mark directory as excluded`
+so you can set `.next` folder `mark directory as` / `excluded`
+
+1. `src/pages` folder is Link Root Path
+so you can set `src/pages` folder `mark directory as` / `Resource Root`
 
 2. Javascript language version set `Flow`
    > [Preferences / Languages & Frameworks / javascript language version](https://blog.jetbrains.com/webstorm/2016/11/using-flow-in-webstorm/)
--- src/pages/examples/index.js
+++ src/pages/examples/index.js
@@ -2,27 +2,23 @@ import React from 'react'
 import Link from 'next/link'
 
 const Examples = () => (
-    <main>
-        <div>
-            <h1>索引頁面</h1>
-            <ul>
-                <li>
-                    <Link href='/examples/withFlowType'><a>with FlowType</a></Link>
-                </li>
-                <li>
-                    <Link href='/examples/withReactI18Next'><a>with React I18Next</a></Link>
-                </li>
-                <li>
-                    <Link href='/examples/withNextI18Next'><a>with Nextjs I18Next</a></Link>
-                </li>
-            </ul>
-        </div>
-
-    </main>
-)
-
-Examples.getInitialProps = async () => ({
-    namespacesRequired: ['common'],
-});
+    <div>
+        <h1>索引頁面</h1>
+        <ul>
+            <li>
+                <Link href='/examples/withFlowType'><a>with FlowType</a></Link>
+            </li>
+            <li>
+                <Link href='/examples/withReactI18Next'><a>with React I18Next</a></Link>
+            </li>
+            <li>
+                <Link href='/examples/withNextI18Next'><a>with Nextjs I18Next</a></Link>
+            </li>
+            <li>
+                <Link href='/examples/withBootstrap'><a>with Bootstrap</a></Link>
+            </li>
+        </ul>
+    </div>
+);
 
 export default Examples;

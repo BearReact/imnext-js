d89e3f8f57268e71ff9ea4a875998896182cc7dd
Imagine Chiu <chihfan.chiu@igroup.tw>
2019-12-17 14:58:20 +0800

    test2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,12 @@ import React, {useEffect} from 'react';
 import Layout from '@layouts/main';
 import Router from 'next/router';
 
-const Homepage = () => {
-
-    useEffect(() => {
-        Router.push('/example');
-    }, []);
-
-    return (
-        <main>Nextjs 9</main>
-    );
-};
+const Homepage = () => (
+    <main>
+        <span>Nextjs 9</span>
+        <button type="button" onClick={() => Router.push('/example')}>Go Sample</button>
+    </main>
+);
 Homepage.Layout = Layout;
 
 export default Homepage;
